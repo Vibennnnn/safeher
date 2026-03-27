8c0ca48e40e1d9ba77b5758695c8262c3f0e1332
Vibennnnn <gamerboyroblox745@gmail.com>
2026-03-26 21:55:05 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'safeher-v1';
-const FILES = ['./', './index.html', './manifest.json'];
+const CACHE = 'safeher-v2';
+const FILES = ['./', './index.html', './manifest.json', './reflection.m4a'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
